16f6a898f1781b1bc128ccb186244acd9e22e5f6
jonatan <jonatan@192.168.0.10>
2021-04-20 22:31:00 -0300

    update

---
+++ src/udemyUniversidadJava/bienvenido.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package udemyUniversidadJava;
+
+/**
+ * @author jonatan
+ *
+ */
+public class bienvenido {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
